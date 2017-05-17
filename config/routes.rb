ad118c03089572e9f9e6ecda17aeacf5516074cd
Unknown <liuhaoxdu@gmail.com>
2017-05-17 19:50:19 +0200

    Return back to version 3.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  resources :registrations
   get 'show/admins'
 
   get 'show/profs'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170517141431) do
+ActiveRecord::Schema.define(version: 20170512164529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,15 +25,6 @@ ActiveRecord::Schema.define(version: 20170517141431) do
     t.integer  "subject_id"
     t.index ["subject_id"], name: "index_exams_on_subject_id", using: :btree
     t.index ["user_id"], name: "index_exams_on_user_id", using: :btree
-  end
-
-  create_table "registrations", force: :cascade do |t|
-    t.string   "email"
-    t.string   "password"
-    t.string   "first_name"
-    t.string   "last_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "scores", force: :cascade do |t|
