db49ec31e20869824a9af9357c0d7cbe7a485c10
Unknown <liuhaoxdu@gmail.com>
2017-05-23 15:28:45 +0200

    Update

--- db/migrate/20170508133429_add_student_to_users.rb
+++ db/migrate/20170508133429_add_student_to_users.rb
@@ -1,5 +1,6 @@
 class AddStudentToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :student, :boolean, default: false
+    add_column :users, :student, :boolean, default: true
+
   end
 end
