0b589689083407a5d0e23d65b4ec5ec9160888a1
Unknown <liuhaoxdu@gmail.com>
2017-05-23 14:22:59 +0200

    Apply to update

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -29,7 +29,7 @@ class ScoresController < ApplicationController
 
     respond_to do |format|
       if @score.save
-        format.html { redirect_to @score, notice: 'Score was successfully created.' }
+        format.html { redirect_to scores_path, notice: 'Score was successfully created.' }
         format.json { render :show, status: :created, location: @score }
       else
         format.html { render :new }
